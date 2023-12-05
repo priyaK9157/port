d8b6e4d17a1ca991b76197a9affac5637beded64
priyaK9157 <priyakumari2003004@gmail.com>
2023-12-05 16:25:02 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+// pages/index.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ backgroundColor: 'white', color: 'black' }}>
+      <h1>Welcome to My Portfolio</h1>
+    </div>
+  );
+};
+
+export default Home;
